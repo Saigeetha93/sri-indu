1f40dd4f09fdae8dcbb07f8f34a175ba86c47449
Lenovo <Lenovo@vamshi.Dlink>
2024-07-09 09:59:33 +0530

    commit message

---
+++ Corejavaprograms/src/com/tnsif/inheritance/Supermain.java
@@ -0,0 +1,13 @@
+package com.tnsif.inheritance;
+
+public class Supermain {
+	public static void main(String[] args) {
+		Student s=new Student("sai",655236931,"bangalore",984509434);
+		System.out.println(s);
+		Student s1=new Student(1,"sriindhu");
+		System.out.println(s1);
+		
+		
+	}
+
+}
